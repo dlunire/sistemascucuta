b9f12bb7ce691b3b3833cd975228d1eab8270c3d
Códigos del Futuro <dlunireframework@gmail.com>
2025-04-10 11:59:25 -0500

    Update

--- src/layers/sections/header/Header.svelte
+++ src/layers/sections/header/Header.svelte
@@ -5,10 +5,11 @@
     import type { Menu as MenuItem } from "../../../lib/interface/menu";
     import type { Size } from "../../../lib/interface/size";
     import { install } from "../../../lib/store";
-    import { onDestroy } from "svelte";
+    import { onDestroy, onMount } from "svelte";
 
     export let menu: MenuItem[] = [];
 
+    let headerElement: HTMLElement | null = null;
     let open: boolean = false;
 
     let size: Size = {
@@ -21,17 +22,34 @@
         console.log({ currentSize });
     });
 
+    /**
+     * Carga el tamaño del elemento seleccionado
+     *
+     * @param element Element a ser analizado
+     * @returns
+     */
+    function loadSize(element: HTMLElement | null): void {
+        if (!(element instanceof HTMLElement)) {
+            return;
+        }
+
+        const sizes = element.getBoundingClientRect();
+        size.width = sizes.width;
+        size.height = sizes.height;
+    }
+
     onDestroy(() => {
         unsubscribe();
     });
 
-    $: top = size.height;
-
-    $: console.log({ top });
+    $: loadSize(headerElement);
+    $: top = size.height * -1;
 </script>
 
-<header class="header" style="--top: {top}px">
-    <nav class="header__social-media"></nav>
+<header class="header" style="--top: {top}px" bind:this={headerElement}>
+    <nav class="header__social-media">
+        <div class="header__social-inner">sdfasd</div>
+    </nav>
 
     <nav class="header__nav header__nav--red" class:header__nav--open={open}>
         <div class="header__logo" class:header__logo--open={open}>
