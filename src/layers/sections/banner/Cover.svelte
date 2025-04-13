f753df87daebabfb5cc77629d730bc89e254c327
Códigos del Futuro <dlunireframework@gmail.com>
2025-04-12 21:25:19 -0500

    Update

--- src/layers/sections/banner/Cover.svelte
+++ src/layers/sections/banner/Cover.svelte
@@ -10,11 +10,21 @@
     import "./splide/splide.scss";
     import "@splidejs/svelte-splide/css/core";
     import { Splide, SplideSlide } from "@splidejs/svelte-splide";
+
     import type { Image } from "../../../lib/interface/Image";
     import { getImages } from "../../../lib/utils/Images";
+    import type { MoveEventDetail } from "@splidejs/svelte-splide/types";
     // import IconPortfolio from "../../icons/IconPortfolio.svelte";
 
     let images: Image[] = getImages();
+    let currentIndex: number = 0;
+
+    function handleMove(event: CustomEvent<MoveEventDetail> | undefined): void {
+        if (!event) return;
+
+        const { index } = event.detail;
+        currentIndex = index;
+    }
 </script>
 
 <section class="banner" id="home">
@@ -31,18 +41,23 @@
             flickPower: 600,
             flickMaxPages: 1,
         }}
+        on:move={handleMove}
     >
         {#each images as { src, title, alt }, index}
             <SplideSlide>
                 <img {src} {alt} />
 
                 <div class="banner__content">
-                    <div class="banner__content__inner">
+                    <div
+                        class="banner__content__inner"
+                        class:active={index == currentIndex}
+                        class:inactive={index != currentIndex}
+                    >
                         <h2 class="banner__title">
                             <span>{title}</span>
                         </h2>
 
-                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>{title}.</p>
                     </div>
                 </div>
             </SplideSlide>
@@ -54,5 +69,20 @@
     .banner {
         min-height: 400px;
         display: flex;
+    }
+
+    .active {
+        background-color: purple;
+        animation-name: fade-in;
+        animation-duration: 400ms;
+        animation-timing-function: ease;
+        animation-delay: 100ms;
+        animation-fill-mode: forwards;
+        opacity: 0;
+        transform: scale(0);
+    }
+
+    .inactive {
+        animation: 300ms ease fade-out forwards;
     }
 </style>
