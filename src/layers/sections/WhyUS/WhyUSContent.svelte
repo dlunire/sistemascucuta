8fed913c6a25ff1077d472809b22daa49b6883a3
Códigos del Futuro <dlunireframework@gmail.com>
2025-04-15 13:45:51 -0500

    Update

--- src/layers/sections/WhyUS/WhyUSContent.svelte
+++ src/layers/sections/WhyUS/WhyUSContent.svelte
@@ -1,7 +1,73 @@
 <script lang="ts"></script>
 
 <!-- Columna de la izquierda en WhyUS -->
-<div class="section__content-column">Columna</div>
+<div class="section__content-column">
+    <details>
+        <summary>Diseño Web</summary>
+        <p>
+            <strong>En resumen:</strong>No solo desarrollamos páginas web;
+            diseñamos experiencias digitales de alto nivel, respaldadas por un
+            equipo experto y comprometido con el éxito de tu presencia en línea.
+            ¡Transforma tu idea en una web profesional desde hoy!
+        </p>
+    </details>
+
+    <details>
+        <summary>Asesoría en Marketing Digital</summary>
+        <p class="margin-0">
+            <strong
+                >Impulsa tu negocio con nuestra asesoría en Marketing Digital</strong
+            >
+        </p>
+        <p>
+            En un entorno digital en constante cambio, no es suficiente con
+            simplemente estar en línea: necesitas destacar. Nuestra asesoría
+            estratégica en marketing digital está diseñada para fortalecer tu
+            marca, aumentar tus conversiones y maximizar tu presencia online con
+            acciones concretas y medibles.
+        </p>
+    </details>
+
+    <details>
+        <summary>Software Contable POS</summary>
+        <p>
+            Ventajas de trabajar con nuestro software de Inventario, Caja y POS
+            En la gestión de un negocio, contar con un sistema eficiente y
+            confiable marca la diferencia. Nuestro software de Inventario, Caja
+            y POS ha sido diseñado para optimizar tu operación, aumentar la
+            productividad y mejorar la rentabilidad de tu empresa, de forma
+            Economica .
+        </p>
+    </details>
+
+    <details>
+        <summary>Licencias Originales</summary>
+        <p>
+            No pongas en riesgo tu equipo y tu información. Compra licencias
+            Originales con nosotros y disfruta de un software confiable, seguro
+            y siempre actualizado.
+        </p>
+
+        <ul>
+            <li>✅Licenia de Microsoft Office 365 x 1 año.</li>
+            <li>✅ Licencia de Windows 10 u 11 Profesional.</li>
+            <li>✅ Licencia de Microsoft Office 2021 Original.</li>
+            <li>✅ Licencia de Software de Virtual DJ.</li>
+            <li>✅ Licencia de Software Autodeks, como AUTOCAD.</li>
+        </ul>
+    </details>
+</div>
 
 <!-- Barra lateral derecha en WhyUS -->
 <aside class="section__content-sidebar">Aside</aside>
+
+<style lang="scss">
+    .margin {
+        &-0 {
+            margin: 0;
+        }
+    }
+
+    details {
+    }
+</style>
