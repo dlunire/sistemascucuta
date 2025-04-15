4d350442bfb140eaa2ce59ea4ed5aff16705e848
Códigos del Futuro <dlunireframework@gmail.com>
2025-04-15 12:39:51 -0500

    Update

--- src/layers/sections/WhyUS/WhyUS.svelte
+++ src/layers/sections/WhyUS/WhyUS.svelte
@@ -1,4 +1,7 @@
-<script lang="ts"></script>
+<script lang="ts">
+    import IconComputer from "../../icons/IconComputer.svelte";
+    import IconWebDesign from "../../icons/IconWebDesign.svelte";
+</script>
 
 <section class="section section--why-us" id="about">
     <div class="section__inner">
@@ -14,41 +17,62 @@
         >
             <div class="section__content-item">
                 <header class="section__content-header">
-                    <img src="./images/slide01.png" alt="Imagen de prueba" />
+                    <IconWebDesign />
                 </header>
 
                 <div class="section__content-content">
-                    <h3 class="section__content-title">Título</h3>
+                    <h3 class="section__content-title">Diseño Web</h3>
+                    <p>
+                        ¡Convierte visitantes en clientes con un diseño web
+                        atractivo y funcional!
+                    </p>
                 </div>
             </div>
 
             <div class="section__content-item">
                 <header class="section__content-header">
-                    <img src="./images/slide01.png" alt="Imagen de prueba" />
+                    <IconComputer />
                 </header>
 
                 <div class="section__content-content">
-                    <h3 class="section__content-title">Título</h3>
+                    <h3 class="section__content-title">
+                        Reparación de computadoras
+                    </h3>
+                    <p>
+                        ¡Mejoramos el rendimiento de tu PC con mantenimiento
+                        profesional!
+                    </p>
                 </div>
             </div>
 
             <div class="section__content-item">
                 <header class="section__content-header">
-                    <img src="./images/slide01.png" alt="Imagen de prueba" />
+                    <img src="./images/icons/i3.png" alt="Asesoría y Cursos" />
                 </header>
 
                 <div class="section__content-content">
-                    <h3 class="section__content-title">Título</h3>
+                    <h3 class="section__content-title">Asesoría y Cursos</h3>
+                    <p>
+                        ¡Aprende con expertos y lleva tu conocimiento al
+                        siguiente nivel!
+                    </p>
                 </div>
             </div>
 
             <div class="section__content-item">
                 <header class="section__content-header">
-                    <img src="./images/slide01.png" alt="Imagen de prueba" />
+                    <img
+                        src="./images/icons/i4.png"
+                        alt="Soluciones en Redes"
+                    />
                 </header>
 
                 <div class="section__content-content">
-                    <h3 class="section__content-title">Título</h3>
+                    <h3 class="section__content-title">Soluciones en Redes</h3>
+                    <p>
+                        ¡Diseñamos e implementamos redes seguras y eficientes
+                        para tu negocio!
+                    </p>
                 </div>
             </div>
         </div>
