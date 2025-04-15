69039424a510fdb85169879e3311105bc76a68c1
Códigos del Futuro <dlunireframework@gmail.com>
2025-04-15 14:08:54 -0500

    Update

--- src/layers/sections/WhyUS/WhyUSContent.svelte
+++ src/layers/sections/WhyUS/WhyUSContent.svelte
@@ -6,57 +6,67 @@
 <div class="section__content-column">
     <details class="why">
         <summary class="why__title">Diseño Web</summary>
-        <p>
-            <strong>En resumen:</strong>No solo desarrollamos páginas web;
-            diseñamos experiencias digitales de alto nivel, respaldadas por un
-            equipo experto y comprometido con el éxito de tu presencia en línea.
-            ¡Transforma tu idea en una web profesional desde hoy!
-        </p>
+        <section class="why__content">
+            <p>
+                <strong>En resumen:</strong><br />No solo desarrollamos páginas
+                web; diseñamos experiencias digitales de alto nivel, respaldadas
+                por un equipo experto y comprometido con el éxito de tu
+                presencia en línea. ¡Transforma tu idea en una web profesional
+                desde hoy!
+            </p>
+        </section>
     </details>
 
     <details class="why">
         <summary class="why__title">Asesoría en Marketing Digital</summary>
-        <p class="margin-0">
-            <strong
-                >Impulsa tu negocio con nuestra asesoría en Marketing Digital</strong
-            >
-        </p>
-        <p>
-            En un entorno digital en constante cambio, no es suficiente con
-            simplemente estar en línea: necesitas destacar. Nuestra asesoría
-            estratégica en marketing digital está diseñada para fortalecer tu
-            marca, aumentar tus conversiones y maximizar tu presencia online con
-            acciones concretas y medibles.
-        </p>
+        <section class="why__content">
+            <p class="margin-0">
+                <strong
+                    >Impulsa tu negocio con nuestra asesoría en Marketing
+                    Digital</strong
+                >
+            </p>
+            <p>
+                En un entorno digital en constante cambio, no es suficiente con
+                simplemente estar en línea: necesitas destacar. Nuestra asesoría
+                estratégica en marketing digital está diseñada para fortalecer
+                tu marca, aumentar tus conversiones y maximizar tu presencia
+                online con acciones concretas y medibles.
+            </p>
+        </section>
     </details>
 
     <details class="why">
         <summary class="why__title">Software Contable POS</summary>
-        <p>
-            Ventajas de trabajar con nuestro software de Inventario, Caja y POS
-            En la gestión de un negocio, contar con un sistema eficiente y
-            confiable marca la diferencia. Nuestro software de Inventario, Caja
-            y POS ha sido diseñado para optimizar tu operación, aumentar la
-            productividad y mejorar la rentabilidad de tu empresa, de forma
-            Economica .
-        </p>
+        <section class="why__content">
+            <p>
+                Ventajas de trabajar con nuestro software de Inventario, Caja y
+                POS En la gestión de un negocio, contar con un sistema eficiente
+                y confiable marca la diferencia. Nuestro software de Inventario,
+                Caja y POS ha sido diseñado para optimizar tu operación,
+                aumentar la productividad y mejorar la rentabilidad de tu
+                empresa, de forma Economica .
+            </p>
+        </section>
     </details>
 
     <details class="why">
         <summary class="why__title">Licencias Originales</summary>
-        <p>
-            No pongas en riesgo tu equipo y tu información. Compra licencias
-            Originales con nosotros y disfruta de un software confiable, seguro
-            y siempre actualizado.
-        </p>
+        <section class="why__content">
+            <p>
+                No pongas en riesgo tu equipo y tu información. Compra licencias
+                Originales con nosotros y disfruta de un software confiable,
+                seguro y siempre actualizado.
+            </p>
 
-        <ul>
-            <li>✅Licenia de Microsoft Office 365 x 1 año.</li>
-            <li>✅ Licencia de Windows 10 u 11 Profesional.</li>
-            <li>✅ Licencia de Microsoft Office 2021 Original.</li>
-            <li>✅ Licencia de Software de Virtual DJ.</li>
-            <li>✅ Licencia de Software Autodeks, como AUTOCAD.</li>
-        </ul>
+            <ul>
+                <li>✅Licenia de Microsoft Office 365 x 1 año.</li>
+                <li>✅ Licencia de Windows 10 u 11 Profesional.</li>
+                <li>✅ Licencia de Microsoft Office 2021 Original.</li>
+                <li>✅ Licencia de Software de Virtual DJ.</li>
+                <li>✅ Licencia de Software Autodeks, como AUTOCAD.</li>
+            </ul>
+        </section>
     </details>
 </div>
 
@@ -68,8 +78,5 @@
         &-0 {
             margin: 0;
         }
-    }
-
-    details {
     }
 </style>
