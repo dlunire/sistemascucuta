f3028df81acb797e7f3f8af0897113bc54defed2
Códigos del Futuro <dlunireframework@gmail.com>
2025-04-17 16:14:30 -0500

    Update

--- src/layers/sections/teams/Team.svelte
+++ src/layers/sections/teams/Team.svelte
@@ -18,26 +18,15 @@
 
         <section class="section__content section__content--team">
             <div class="team">
-                <Profile title="David Correa" image="david-correa.jpeg">
-                    <span>Soporte Técnico Informático</span>
-                </Profile>
+                <Profile title="Alvaro Mantilla" image="alvaro-mantilla.jpeg"
+                ></Profile>
 
                 <Profile title="David E Luna M" image="dlunamontilla.jpeg">
                     <span>Software Developer</span>
                 </Profile>
 
-                <Profile
-                    title="Francisco Berrera"
-                    image="francisco-becerra.png"
-                >
-                    <span>Web Master</span>
-                </Profile>
-
-                <Profile
-                    title="Sergio Vargas"
-                    image="sergio-vargas-profile.webp"
-                >
-                    <span>Ingeniero de Sistemas</span>
+                <Profile title="David Correa" image="david-correa.jpeg">
+                    <span>Laboratorio electrónico</span>
                 </Profile>
             </div>
         </section>
